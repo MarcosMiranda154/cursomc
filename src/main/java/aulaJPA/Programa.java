17fe5f5bf04d3fc11d35ee2a510d4514ea349c9a
Marcos Miranda <mvms154@gmail.com>
2020-10-31 14:50:15 -0300

    Banco de dados H@ e criação automatica da base de dados

---
+++ src/main/java/aulaJPA/Programa.java
@@ -0,0 +1,17 @@
+package aulaJPA;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(1, "Carlos", "Carlos@gmail.com");
+		Pessoa p2 = new Pessoa(1, "Marcos", "Marcos@gmail.com");
+		Pessoa p3 = new Pessoa(1, "Vinícius", "Vinicius@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+	}
+
+}
